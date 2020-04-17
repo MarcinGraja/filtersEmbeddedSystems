28965b618291223e16c8ef923ea558a51dcb21b4
Marcin <marcin.graja@wp.pl>
2020-04-17 22:17:10 +0200

    done I/O

--- src/WriterCSV.java
+++ src/WriterCSV.java
@@ -12,7 +12,35 @@ public class WriterCSV {
             return;
         }
         try {
-            bufferedWriter.write(data.getOutAsCSV());
+
+            bufferedWriter.write("true angle," +
+                    "time[ms]," +
+                    "gyro," +
+                    "accelerometer," +
+                    "filtr komplementarny (żyroskop + akcelerometr)," +
+                    "filtr kroczący; średnia z 5 ostatnich pomiarów (akcelerometr)," +
+                    "filtr kroczący; średnia z 10 ostatnich pomiarów (akcelerometr)\n");
+            double[] times = data.getTimes();
+            double[] trueAngles = data.getTrueAngles();
+            double[] gyro = data.getGyro();
+            double[] accelerator = data.getAccelerator();
+            double[] complimentaryFilter = data.getComplimentaryFilter();
+            double[] step5Filter = data.getStep5Filter();
+            double[] step10Filter = data.getStep10Filter();
+            for (int i = 0; i < times.length; i++){
+                String string = trueAngles[i] + "," +
+                        times[i] + "," +
+                        gyro[i] + "," +
+                        accelerator[i] + "," +
+                        complimentaryFilter[i] + "," +
+                        step5Filter[i] + "," +
+                        step10Filter[i] + "\n";
+                bufferedWriter.write(string);
+                if (i > 700){
+                    continue;
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
