7ccbe1b17f49a5c71803ed19dc6ecd684930f614
Marcin <marcin.graja@wp.pl>
2020-04-17 18:03:38 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import javax.print.DocFlavor;
+
+public class Main {
+    public static void main(String[] args){
+        ReaderCSV readerCSV = new ReaderCSV();
+        readerCSV.read();
+    }
+}
